82e43fd280e068acf6074180eca49d37e7293a1b
atkinsonholly <atkinsonholly@gmail.com>
2018-11-27 17:35:10 +0000

    Tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr Chocolate", 5, "M", "Brown", "Cuddly", 1),
+("Rowdy", 2, "M", "Brown", "Cuddly", 1),
+("Tabitha", 1, "F", "Brown", "Cuddly", 1),
+("Sergeant Brown", 5, "M", "Brown", "Cuddly", 1),
+("Melissa", 2, "F", "Brown", "Cuddly", 0),
+("Grinch", 7, "M", "Brown", "Aggressive", 1),
+("Wendy", 5, "F", "Brown", "Cuddly", 1),
+(NULL, 6, "M", "Brown", "Aggressive", 0);
