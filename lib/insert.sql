696872cda79e5c57cfa0ecc73315fca669d321af
morwen80 <ribesnero80@gmail.com>
2018-11-28 12:00:32 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,30 @@
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, "Male", "brown", "jolly", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, "Male", "brown", "rowdy", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 10, "Female", "black", "angry", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, "Male", "golden", "jolly", 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, "Female" "brown", "jolly", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, "Male" "white", "mischevious", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 11, "Female" "black", "jolly", 0);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ( NULL, 1, "Female" "red", "jolly", 0);
+
+
+-- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+-- (1,"Mr. Chocolate", 2, "Male", "brown", "jolly", 1),
+-- (2,"Rowdy", 10, "Male", "black", "rowdy", 1),
+-- (3,"Tabitha", 2, "Female", "black", "nice", 1),
+-- (4,"Sargeant Brown", 7, "Male", "golden", "mischevious", 0),
+-- (5,"Melissa", 8, "Female", "brown", "lazy", 1),
+-- (6,"Grinch", 2, "Male", "white", "always-", 1),
+-- (7,"Wendy", 10, "Female", "black", "naive", 1),
+-- (8,null, 1, "Female", "red", "angry", 0);
+
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
